b20a3cc455bf3fffd073a9aad1fa7e6ab96a16ad
Fernando González Cortés <fernando.gonzalez@geomati.co>
2018-10-16 15:43:29 +0200

    initial commit

---
+++ src/test/java/org/fergonco/blog/springformbasedauth/SpringFormBasedAuthApplicationTests.java
@@ -0,0 +1,16 @@
+package org.fergonco.blog.springformbasedauth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringFormBasedAuthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
